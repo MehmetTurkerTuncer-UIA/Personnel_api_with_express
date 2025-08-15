95355e2972a44377e79e7d6b3ba11625e12271cf
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-15 22:07:32 +0200

    departmentController

--- src/controllers/departmentController.js
+++ src/controllers/departmentController.js
@@ -1,6 +1,55 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-const Department = require('../models/department.model')
+const Department = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Department);
+
+    res.status(200).send({
+      error: false,
+      result: data,
+
+      detail: await res.getModelDetails(Department),
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await Department.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await Department.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await Department.updateOne({ _id: req.params.id }, req.body);
+
+    res.status(202).send({
+      error: false,
+      result: data,
+      new: await Department.updateOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      result: data,
+    });
+  },
+};
