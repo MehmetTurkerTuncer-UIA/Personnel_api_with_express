e377fbc684edb7e50def10fb18d29814e64050ae
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-16 00:04:10 +0200

    personnelController

--- index.js
+++ index.js
@@ -18,17 +18,66 @@ const app = express();
 require("dotenv").config()
 const PORT = process.env?.PORT || 8000
 
-/*------------------------------------------------------- */
-
-
-
 // asyncErrors to errorHandler:
 require("express-async-errors");
 
 /*------------------------------------------------------- */
+dbConnection()
+
+/*------------------------------------------------------- */
+app.use(express.json())
+
+/*------------------------------------------------------- */
+app.use(
+  require("cookie-session")({
+    secret: process.env.SECRET_KEY,
+    // cookie: {
+    //     secure: !(process.env.NODE_ENV=="development"),
+    //     httpOnly: false,
+    //     maxAge: 24 * 60 * 60 * 1000,
+    //   }
+  }),
+);
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+/*------------------------------------------------------- */
+
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PERSONNEL API",
+    session: req.session,
+    isLogin: req.isLogin,
+  });
+});
+
+/*------------------------------------------------------- */
+app.use("/departments", require("./src/routes/department.router"))
+
+/*------------------------------------------------------- */
+
+//personnel
+app.use("/personnels", require("./src/routes/personnel.router"));
+
+/*------------------------------------------------------- */
 
 
+//not found routes
+app.all("*", async (req, res) => {
+  res.status(404).send({
+    error: true,
+    message: "Route not available",
+  });
+});
+/*------------------------------------------------------- */
 
+// errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
+
+/*------------------------------------------------------- */
 
 // RUN SERVER:
 app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -6,6 +6,19 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 
 
+const department = require('../controllers/departmentController')
+
+
+router.route("/")
+    .get(department.list)
+    .post(department.create)
+/* ------------------------------------------------------- */
+router.route("/:id")
+    .get(department.read)
+    .put(department.update)
+    .patch(department.update)
+    .delete(department.delete)
+    
 
 /* ------------------------------------------------------- */
 module.exports = router
